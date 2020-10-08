2f661f913f53bca15c6f11b4bffd4cc42ad0bc68
Muhammad Sharjeel Maqsood <msmaqsood.bscs19seecs@seecs.edu.pk>
2020-10-08 19:38:10 +0500

    Components 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
+import MenuComponent from './components/MenuComponent';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>
+      </Navbar>
+      <MenuComponent />
+
     </div>
   );
 }
