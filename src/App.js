11051ce1b83203a0e39f73ca44431972cf9c2085
Muhammad Sharjeel Maqsood <msmaqsood.bscs19seecs@seecs.edu.pk>
2020-10-08 19:55:54 +0500

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap'
 import './App.css';
 import MenuComponent from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-      </Navbar>
-      <MenuComponent />
 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+        </Navbar>
+        <MenuComponent dishes={this.state.dishes}/>
+  
+      </div>
+    );
+  }
+
 }
-
 export default App;
