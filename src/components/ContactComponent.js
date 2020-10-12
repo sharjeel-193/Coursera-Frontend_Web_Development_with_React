aef65287f448b78d8058e162fa29b5ad7237552d
Muhammad Sharjeel Maqsood <msmaqsood.bscs19seecs@seecs.edu.pk>
2020-10-12 12:07:38 +0500

    Controlled Forms Updated

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,54 +1,112 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import { Breadcrumb, BreadcrumbItem,
-    Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+    Button, Form, FormGroup, Label, Input, Col, Row, FormFeedback } from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-function ContactComponent(props) {
 
-    const [firstName, setfirstName] = useState('');
-    const [lastName, setlastName] = useState('');
-    const [email, setemail] = useState('');
-    const [telnum, settelnum] = useState('');
-    const [agree, setagree] = useState(false);
-    const [contactType, setcontactType] = useState("Tel.")
-    const [message, setmessage] = useState('');
 
-    const handleInputChange = (event) => {
-        // const target = event.target;
-        // const value = target.type === 'checkbox' ? target.checked: target.value;
-        // const name = target.name;
-        
+export default class ContactComponent extends Component {
+    
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: '',
+            touched: {
+                firstname: false,
+                lastname: false,
+                telnum: false,
+                email: false
+            }
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
     }
-    const handleSubmit = (event) => {
-        // event.target.reset();
+    
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
     }
 
-    return(
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Contact Us</h3>
-                    <hr />
-                </div>                
-            </div>
-            <div className="row row-content">
+    handleSubmit(event) {
+        console.log('Current State is: ' + JSON.stringify(this.state));
+        alert('Current State is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+    
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: { ...this.state.touched, [field]: true }
+        });
+    }
+
+    validate(firstname, lastname, telnum, email) {
+        const errors = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: ''
+        };
+
+        if (this.state.touched.firstname && firstname.length < 3)
+            errors.firstname = 'First Name should be >= 3 characters';
+        else if (this.state.touched.firstname && firstname.length > 10)
+            errors.firstname = 'First Name should be <= 10 characters';
+
+        if (this.state.touched.lastname && lastname.length < 3)
+            errors.lastname = 'Last Name should be >= 3 characters';
+        else if (this.state.touched.lastname && lastname.length > 10)
+            errors.lastname = 'Last Name should be <= 10 characters';
+
+        const reg = /^\d+$/;
+        if (this.state.touched.telnum && !reg.test(telnum))
+            errors.telnum = 'Tel. Number should contain only numbers';
+
+        if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
+            errors.email = 'Email should contain a @';
+
+        return errors;
+    }
+    
+    render() {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Contact Us</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row row-content">
                    <div className="col-12">
                       <h3>Send us your Feedback</h3>
                    </div>
                     <div className="col-12 col-md-9">
-                        <Form onSubmit={() => console.log(firstName)}>
+                        <Form onSubmit={this.handleSubmit}>
                             <FormGroup row>
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
                                     <Input type="text" id="firstname" name="firstname"
                                         placeholder="First Name"
-                                        value={firstName}
-                                        onChange={e => setfirstName(e.target.value)}
-                                        />
+                                        value={this.state.firstname}
+                                        onChange={this.handleInputChange} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -56,8 +114,8 @@ function ContactComponent(props) {
                                 <Col md={10}>
                                     <Input type="text" id="lastname" name="lastname"
                                         placeholder="Last Name"
-                                        value={lastName}
-                                        onChange={e => setlastName(e.target.value)} />
+                                        value={this.state.lastname}
+                                        onChange={this.handleInputChange} />
                                 </Col>                        
                             </FormGroup>
                             <FormGroup row>
@@ -65,8 +123,8 @@ function ContactComponent(props) {
                                 <Col md={10}>
                                     <Input type="tel" id="telnum" name="telnum"
                                         placeholder="Tel. number"
-                                        value={telnum}
-                                        onChange={e => settelnum(e.target.value)} />
+                                        value={this.state.telnum}
+                                        onChange={this.handleInputChange} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -74,8 +132,8 @@ function ContactComponent(props) {
                                 <Col md={10}>
                                     <Input type="email" id="email" name="email"
                                         placeholder="Email"
-                                        value={email}
-                                        onChange={e => setemail(e.target.value)} />
+                                        value={this.state.email}
+                                        onChange={this.handleInputChange} />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -84,16 +142,16 @@ function ContactComponent(props) {
                                         <Label check>
                                             <Input type="checkbox"
                                                 name="agree"
-                                                checked={agree}
-                                                onChange={e => setagree(e.target.checked?true:false)} /> {' '}
+                                                checked={this.state.agree}
+                                                onChange={this.handleInputChange} /> {' '}
                                             <strong>May we contact you?</strong>
                                         </Label>
                                     </FormGroup>
                                 </Col>
                                 <Col md={{size: 3, offset: 1}}>
                                     <Input type="select" name="contactType"
-                                            value={contactType}
-                                            onChange={e => setcontactType(e.target.value)}>
+                                            value={this.state.contactType}
+                                            onChange={this.handleInputChange}>
                                         <option>Tel.</option>
                                         <option>Email</option>
                                     </Input>
@@ -104,8 +162,8 @@ function ContactComponent(props) {
                                 <Col md={10}>
                                     <Input type="textarea" id="message" name="message"
                                         rows="12"
-                                        value={message}
-                                        onChange={e => setmessage(e.target.value)}></Input>
+                                        value={this.state.message}
+                                        onChange={this.handleInputChange}></Input>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
@@ -114,40 +172,39 @@ function ContactComponent(props) {
                                         Send Feedback
                                     </Button>
                                 </Col>
-                                
                             </FormGroup>
                         </Form>
                     </div>
                </div>
-            <div className="row row-content">
-                <div className="col-12">
-                <h3>Location Information</h3>
-                </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
-                        <h5>Our Address</h5>
-                        <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
-                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                        </address>
-                </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+
+                <div className="row row-content">
+                    <div className="col-12">
+                    <h3>Location Information</h3>
                     </div>
-                    
+                    <div className="col-12 col-sm-4 offset-sm-1">
+                            <h5>Our Address</h5>
+                            <address>
+                            121, Clear Water Bay Road<br />
+                            Clear Water Bay, Kowloon<br />
+                            HONG KONG<br />
+                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            </address>
+                    </div>
+                    <div className="col-12 col-sm-6 offset-sm-1">
+                        <h5>Map of our Location</h5>
+                    </div>
+                    <div className="col-12 col-sm-11 offset-sm-1">
+                        <div className="btn-group" role="group">
+                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-
-export default ContactComponent;
