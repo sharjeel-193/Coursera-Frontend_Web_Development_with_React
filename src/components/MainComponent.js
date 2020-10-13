cd5fd618acdbbcd4fe7d1e288c68637fbf034868
Muhammad Sharjeel Maqsood <msmaqsood.bscs19seecs@seecs.edu.pk>
2020-10-13 14:27:16 +0500

    React Redux Form Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,87 +1,82 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Home from './HomeComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
-import Contact from './ContactComponent';
-import About from './AboutComponent'
+import DishdetailComponent from './DishdetailComponent';
+import HeaderComponent from './HeaderComponent';
+import FooterComponent from './FooterComponent';
+import HomeComponent from './HomeComponent';
+import ContactComponent from './ContactComponent';
+import AboutComponent from './AboutComponent';
+import MenuComponent from './MenuComponent';
 
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
-
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
-  return {
-    dishes: state.dishes,
-    comments: state.comments,
-    promotions: state.promotions,
-    leaders: state.leaders
-  }
-}
-const mapDispatchToProps = dispatch => ({
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    };
+};
 
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+const mapDispatchToProps = (dispatch) => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount(){
-    this.props.fetchDishes();
-  }
-
-  render() {
-
-
-    
-    
-    const HomePage = () => {
-      return(
-        <Home 
-          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-          dishesLoading={this.props.dishes.isLoading}
-          dishesErrMess={this.props.dishes.errMess}
-          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-        />
-      );
+    constructor(props) {
+        super(props);
     }
-            
-    const DishWithId = ({match}) => {
-      return(
-          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-            isLoading={this.props.dishes.isLoading}
-            errMess={this.props.dishes.errMess}
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-            addComment={this.props.addComment}
-          />
-      );
+
+    componentDidMount() {
+        this.props.fetchDishes();
     }
-            
-    return (
-      <div>
-        <Header />
-        <div>
-          <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-              <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
-              <Redirect to="/home" />
-          </Switch>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+
+    render() {
+
+        const HomePage = () => {
+            return (
+                <HomeComponent dish={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.filter(promo => promo.featured)[0]}
+                    leader={this.props.leaders.filter(leader => leader.featured)[0]}
+                />
+            );
+        }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishdetailComponent
+                    dish={this.props.dishes.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    comments={this.props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
+                />
+            );
+        }
+
+        return (
+            <div>
+                <HeaderComponent />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <MenuComponent dishes={this.props.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/contactus" component={() => <ContactComponent resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                    <Route exact path="/aboutus" component={() => <AboutComponent leaders={this.props.leaders} />} />
+                    <Redirect to="/home" />
+                </Switch>
+                <FooterComponent />
+            </div>
+        );
+    }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,15 +6,22 @@ import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             dishes: Dishes,
+            comments: Comments,
             promotions: Promotions,
             leaders: Leaders,
-            comments: Comments,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
+        
         applyMiddleware(thunk, logger)
     );
 
